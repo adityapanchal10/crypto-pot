ac8606a61b39595e953b7bdd441778d6c52f3a55
Mohammed Mushahid Qureshi <mushahidq@gmail.com>
2022-01-30 22:54:33 +0530

    redesigned index page

--- index.php
+++ index.php
@@ -7,8 +7,11 @@ include "db_connect.php";
 // If the user is not logged in redirect to the login page...
 if (!isset($_SESSION['email'])) {
 	$login = '<a href="login.php" class="login-btn scrollto">Login</a>';
+  $signup = '<a href="signup.php" class="signup-btn animate__animated animate__fadeInUp scrollto">Register Now</a>';
 } else {
   $login = '<a href="dashboard.php" class="login-btn scrollto">Dashboard</a>';
+  $signup = '<a href="dashboard.php" class="signup-btn animate__animated animate__fadeInUp scrollto">Go to Dashboard/a>';
+
 }
 echo '
 <!DOCTYPE html>
@@ -42,12 +45,10 @@ echo '
   <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
   <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
   <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
-  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
-  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
 
   <!-- Main CSS File -->
   <link href="assets/css/style.css" rel="stylesheet">
-
+  
 </head>
 
 <body>
@@ -72,11 +73,13 @@ echo '
 
       <nav id="navbar" class="navbar order-last order-lg-0">
         <ul>
-          <li><a class="nav-link scrollto" href="#hero">Get Started</a></li>
+          <li><a class="nav-link scrollto" href="#title">Get Started</a></li>
+          <li><a class="nav-link scrollto" href="#invest">Invest</a></li>
           <li><a class="nav-link scrollto" href="#about">About</a></li>
-          <li><a class="nav-link scrollto" href="#prices">Prices</a></li>
+          <li><a class="nav-link scrollto" href="#market">Trends</a></li>
+          <li><a class="nav-link scrollto" href="#prices">Why Us?</a></li>
           <li><a class="nav-link scrollto" href="#crypto">Cryptocurrencies</a></li>
-          <li><a class="nav-link scrollto" href="#wallet">Wallet</a></li>
+          <!-- <li><a class="nav-link scrollto" href="#wallet">Wallet</a></li> -->
           <!-- 
           <li class="dropdown"><a href="#"><span>Drop Down</span> <i class="bi bi-chevron-down"></i></a>
             <ul>
@@ -107,76 +110,97 @@ echo '
   </header>
   <!-- End Header -->
 
-  <!-- ======= Hero Section ======= -->
-  <section id="hero">
-    <div class="hero-container">
-      <div id="heroCarousel" data-bs-interval="5000" class="carousel slide carousel-fade" data-bs-ride="carousel">
+  <main id="main">
 
-        <ol class="carousel-indicators" id="hero-carousel-indicators"></ol>
-
-        <div class="carousel-inner" role="listbox">
-
-          <!-- Slide 1 -->
-          <div class="carousel-item active"
-            style="background: url(assets/img/slide/slide-1.svg); background-repeat: no-repeat; background-size: cover;">
-            <div class="carousel-container">
-              <div class="carousel-content">
-                <h2 class="animate__animated animate__fadeInDown"><span>Anything</span> To Anything</h2>
-                <p class="animate__animated animate__fadeInUp">Trade between cryptocurrencies, equities and national
-                  currencies in one single step.</p>
-                <div>
-                  <a href="signup.php" class="signup-btn animate__animated animate__fadeInUp scrollto">Signup Now</a>
-                </div>
-              </div>
+    <!-- ======= Title Section ======= -->
+    <section id="title">
+      <div class="container">
+        <div class="title-section">
+          <h1>Buy and Sell <span>Crypto</span> in minutes</h1>
+          <h5>Trade between cryptocurrencies, equities and national currencies in one single step.</h5>
+        </div>
+        <div class="title-btn">
+          <button class="signup-btn">
+            '.$signup.'
+          </button>
+        </div>
+        <div class="slider">
+          <div class="slider-item" style="display: none;">
+            <h7 class="slider-title"></h7>
+            <p class="slider-price"></p>
+            <p class="slider-price-change"></p>
+          </div>
+          <!-- <div class="slider-item">
+            <div class="slider-item-content">
+              <h2>Buy and Sell Crypto</h2>
+              <p>Trade between cryptocurrencies, equities and national currencies in one single step.</p>
             </div>
           </div>
-
-          <!-- Slide 2 -->
-          <div class="carousel-item"
-            style="background: url(assets/img/slide/slide-2.svg); background-repeat: no-repeat; background-size: cover;">
-            <div class="carousel-container">
-              <div class="carousel-content">
-                <h2 class="animate__animated animate__fadeInDown"><span>Anything</span> To Anything</h2>
-                <p class="animate__animated animate__fadeInUp">Trade between cryptocurrencies, equities and national
-                  currencies in one single step.</p>
-                <div>
-                  <a href="signup.php" class="signup-btn animate__animated animate__fadeInUp scrollto">Signup Now</a>
-                </div>
-              </div>
+          <div class="slider-item">
+            <div class="slider-item-content">
+              <h2>Buy and Sell Crypto</h2>
+              <p>Trade between cryptocurrencies, equities and national currencies in one single step.</p>
             </div>
           </div>
-
-          <!-- Slide 3 -->
-          <div class="carousel-item"
-            style="background: url(assets/img/slide/slide-3.svg); background-repeat: no-repeat; background-size: cover;">
-            <div class="carousel-container">
-              <div class="carousel-content">
-                <h2 class="animate__animated animate__fadeInDown"><span>Anything</span> To Anything</h2>
-                <p class="animate__animated animate__fadeInUp">Trade between cryptocurrencies, equities and national
-                  currencies in one single step.</p>
-                <div>
-                  <a href="signup.php" class="signup-btn animate__animated animate__fadeInUp scrollto">Signup Now</a>
-                </div>
-              </div>
+          <div class="slider-item">
+            <div class="slider-item-content">
+              <h2>Buy and Sell Crypto</h2>
+              <p>Trade between cryptocurrencies, equities and national currencies in one single step.</p>
             </div>
+          </div> -->
+        </div>
+      </div>
+    </section>
+
+    <!-- ======= Invest Section ======= -->
+    <section id="invest" class="about" style="background: #fff;">
+      <div class="container-fluid">
+
+        <div class="row">
+
+          <div class="col-lg-6 d-flex flex-column justify-content-center align-items-stretch">
+
+            <div class="content">
+              <h3 style="margin-bottom: 20px;"><strong>Invest & Grow your <br />cryptocurrency porfolio</strong></h3>
+              <p style="font-size: 20px; margin-bottom: 40px;">
+                Reasons to invest in cryptocurrencies in 2021
+              </p>
+
+              <ul style="font-size: 20px;">
+                <li style="margin-bottom: 40px;">
+                  <i class="bx bx-check-double"></i> <strong>Buying Bitcoin is legal</strong>
+                  <p class="fst-italic" style="font-size: 16px;">
+                    The Supreme Court judgement turned in favour of Indian<br />
+                    investors making investing in Cryptocurrencies legal.
+                  </p>
+                </li>
+                <li style="margin-bottom: 40px;">
+                  <i class="bx bx-check-double"></i> <strong>Growing alternate asset class</strong>
+                  <p class="fst-italic" style="font-size: 16px;">
+                    Trusted by 70M+ traders, major banks, institutional and<br />
+                    renowned business investors and hedge funds.
+                  </p>
+                </li>
+                <li style="margin-bottom: 40px;">
+                  <i class="bx bx-check-double"></i> <strong>High return potential</strong>
+                  <p class="fst-italic" style="font-size: 16px;">
+                    Bitcoin became the most lucrative choice of investment<br />
+                    delivering a whopping 300%+ returns in 2020.
+                  </p>
+                </li>
+              </ul>
+            </div>
+
+          </div>
+
+          <div class="col-lg-6 align-items-center video-box" style=\'background-image: url("assets/img/pf.png");\'>
           </div>
 
         </div>
 
-        <a class="carousel-control-prev" href="#heroCarousel" role="button" data-bs-slide="prev">
-          <span class="carousel-control-prev-icon bi bi-chevron-left" aria-hidden="true"></span>
-        </a>
-
-        <a class="carousel-control-next" href="#heroCarousel" role="button" data-bs-slide="next">
-          <span class="carousel-control-next-icon bi bi-chevron-right" aria-hidden="true"></span>
-        </a>
-
       </div>
-    </div>
-  </section>
-  <!-- End Hero -->
-
-  <main id="main">
+    </section>
+    <!-- End Invest Section -->
 
     <!-- ======= About Section ======= -->
     <section id="about" class="about">
@@ -184,14 +208,15 @@ echo '
 
         <div class="row">
 
-          <div class="col-lg-5 align-items-stretch video-box" style=\'background-image: url("assets/img/about.png");\'>
+          <div class="col-lg-5 align-items-stretch video-box" style=\'background-image: url("assets/img/about.png");\'
+            style="margin-right: 40px;">
             <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" class="venobox play-btn mb-4" data-vbtype="video"
               data-autoplay="true"></a>
           </div>
 
           <div class="col-lg-7 d-flex flex-column justify-content-center align-items-stretch">
 
-            <div class="content">
+            <div class="content zzz">
               <h3>About our <strong>Crypto Exchange</strong></h3>
               <p>
                 Simply the best place to buy digital currencies
@@ -214,8 +239,51 @@ echo '
     </section>
     <!-- End About Section -->
 
+    <!-- ======= Market Section ======= -->
+    <section id="market" class="about" style="background: #fff;">
+      <div class="container section-title">
+        <h2><span>Market</span> Trend</h2>
+      </div>
+      <div class="container-fluid">
+
+        <div class="row">
+
+          <div class="col-lg-12 d-flex flex-column justify-content-center align-items-flex-start">
+
+            <div class="content mrkt">
+
+              <div class="scrollable-content">
+                <div class="row0">
+                  <div class="logo0"></div>
+                  <div class="title0" style="margin-left: 85px;">Currency</div>
+                  <!-- <div class="symbol0">ID</div> -->
+                  <div class="amount0" style="margin-left: 50px;">Last Price</div>
+                  <div class="change0">24h change</div>
+                  <div class="graph0" style="margin-left:25px;">Market</div>
+                </div>
+                <ul id="dataList" style="list-style-type:none;"></ul>
+                <div class="templates">
+                  <div id="listItem">
+                    <div class="row1">
+                      <img class="img-fluid logo" src="./assets/img/transparent.png" width="0" height="0">
+                      <div class="symbol"></div>
+                      <div class="title"></div>
+                      <div class="amount"></div>
+                      <div class="change"></div>
+                      <img class="img-fluid graph" src="./assets/img/transparent.png" width="0" height="0">
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- End Market Section -->
+
     <!-- ======= Why Us Section ======= -->
-    <section id="prices" class="why-us">
+    <section id="prices" class="why-us" style="background: #fffaf3;">
       <div class="container">
 
         <div class="section-title">
@@ -263,25 +331,12 @@ echo '
 
       </div>
     </section>
-    <!-- End Whu Us Section -->
+    <!-- End Why Us Section -->
 
     <!-- ======= Crypto Section ======= -->
     <section id="crypto" class="menu">
       <div class="container">
-
-        <div class="section-title">
-          <h2><span></span></h2>
-        </div>
-
         <div class="row">
-
-          <!-- <ul id="menu-flters">
-              <li data-filter="*" class="filter-active">Show All</li>
-              <li data-filter=".filter-starters">Starters</li>
-              <li data-filter=".filter-salads">Salads</li>
-              <li data-filter=".filter-specialty">Specialty</li>
-            </ul> -->
-
           <div class="col-lg-3 mt-3 mt-lg-0" align-items="center">
             <div class="box">
               <h2 class="animate__animated animate__fadeInDown" align="center"><span>27</span></h2>
@@ -313,7 +368,7 @@ echo '
 
 
     <!-- ======= Contact Section ======= -->
-    <section id="contact" class="contact">
+    <section id="contact" class="contact" style="background: #fffaf3;">
       <div class="container">
 
         <div class="section-title">
@@ -322,8 +377,8 @@ echo '
         </div>
       </div>
 
-      <div class="map">
-        <iframe width="100%" height="350" style="border:0" loading="lazy" frameborder="0" allowfullscreen
+      <div class="map" style="text-align: center;">
+        <iframe width="80%" height="350" style="border:0" loading="lazy" frameborder="0" allowfullscreen
           src="https://www.google.com/maps/embed/v1/place?q=place_id:ChIJcdSOFk3PXzkRmVCXOQLjW8k&key=AIzaSyBs5HExJTvuIn2TReNZK8n6gHpR_DDGwrg"></iframe>
       </div>
 
@@ -331,19 +386,19 @@ echo '
 
         <div class="info-wrap">
           <div class="row">
-            <div class="col-lg-4 col-md-6 info">
+            <div class="col-lg-4 col-md-6 info" style="background: #fffaf3;">
               <i class="bi bi-geo-alt"></i>
               <h4>Location:</h4>
               <p>Sayajigunj<br>Vadodara, GJ 390002</p>
             </div>
 
-            <div class="col-lg-4 col-md-6 info mt-4 mt-lg-0">
+            <div class="col-lg-4 col-md-6 info mt-4 mt-lg-0" style="background: #fffaf3;">
               <i class="bi bi-envelope"></i>
               <h4>Email:</h4>
               <p>info@example.com<br>contact@example.com</p>
             </div>
 
-            <div class="col-lg-4 col-md-6 info mt-4 mt-lg-0">
+            <div class="col-lg-4 col-md-6 info mt-4 mt-lg-0" style="background: #fffaf3;">
               <i class="bi bi-phone"></i>
               <h4>Call:</h4>
               <p>+91 55555 55555<br>+91 55555 55556</p>
@@ -396,6 +451,12 @@ echo '
       <div class="copyright">
         &copy; Copyright <strong><span>Crypto</span></strong>. All Rights Reserved
       </div>
+      <div class="links">
+        <a href="#">Privacy Policy</a>
+        <a href="#">Cookie Policy</a>
+        <a href="#contact">Feedback</a>
+        <a href="#contact">Report a Problem</a>
+      </div>
     </div>
   </footer>
   <!-- End Footer -->
@@ -404,15 +465,113 @@ echo '
       class="bi bi-arrow-up-short"></i></a>
 
   <!-- Vendor JS Files -->
+  <script src="./assets/vendor/core/jquery.3.2.1.min.js" type="text/javascript"></script>
   <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
-  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
   <script src="assets/vendor/php-email-form/validate.js"></script>
-  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
 
   <!-- Template Main JS File -->
   <script src="assets/js/main.js"></script>
+  <script>
+    https: var list = {
+      url:
+        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=bitcoin,ethereum,dogecoin,matic-network,ripple,terra-luna,litecoin,monero,tether,solana,shiba-inu,uniswap&order=market_cap_desc&per_page=100&page=1&sparkline=true&price_change_percentage=24h",
+      method: "GET",
+      timeout: 0,
+    };
 
+    $.ajax(list).done(function (response) {
+      console.log(response);
+      var dataObject = response;
+      var listItemString = $("#listItem").html();
+
+      dataObject.forEach(buildNewList);
+
+      function buildNewList(item, index) {
+        var listItem = $("<li>" + listItemString + "</li>");
+        // console.log(item.logo_url);
+        var logo = $(".logo", listItem);
+        logo.attr("src", item.image);
+        var listItemTitle = $(".title", listItem);
+        listItemTitle.html(item.name);
+        // console.log(typeof item.current_price);
+        var listItemAmount = $(".amount", listItem);
+        listItemAmount.html("$ " + parseFloat(item.current_price).toFixed(2));
+        var listItemDesc = $(".symbol", listItem);
+        listItemDesc.html(item.symbol.toString().toUpperCase());
+        var listItemChange = $(".change", listItem);
+        listItemChange.html(
+          parseFloat(item.price_change_percentage_24h_in_currency).toFixed(2) + "%"
+        );
+        var listItemGraph = $(".graph", listItem);
+        var data = item.sparkline_in_7d.price;
+        data = data.toString();
+        var src = "https://quickchart.io/chart?bkg=white&c={type:\'sparkline\',data:{datasets:[{backgroundColor:\'rgba(255,0,0,0.2)\',borderColor:\'red\',data:[" + data + "]}]}}"
+        listItemGraph.attr("src", src);
+        // console.log(src);
+        $("#dataList").append(listItem);
+      }
+    });
+
+
+    var crpytoList = ["bitcoin", "ethereum", "dogecoin", "matic-network", "ripple", "terra-luna", "litecoin", "monero", "tether", "solana", "shiba-inu", "uniswap"];
+
+    var random = crpytoList.sort(() => .5 - Math.random()).slice(0, 5);
+
+    random = random.join(",");
+
+    // console.log(random);
+
+    /* https: var list2 = {
+      url:
+        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=bitcoin,ethereum,dogecoin,ripple,solana&order=market_cap_desc&per_page=100&page=1&sparkline=false&price_change_percentage=24h",
+      method: "GET",
+      timeout: 0,
+    }; */
+
+    // Use the following for random cryptos
+    https: var list2 = {
+      url:
+        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=" + random + "&order=market_cap_desc&per_page=100&page=1&sparkline=false&price_change_percentage=24h",
+      method: "GET",
+      timeout: 0,
+    };
+
+    $.ajax(list2).done(function (response) {
+      console.log(response);
+      var dataObject = response;
+      var listItemString = $(".slider-item").html();
+
+      dataObject.forEach(buildNewSlider);
+
+      var cls = "class=\"slider-item\"";
+
+      function buildNewSlider(item, index) {
+        var listItem = $("<div>" + listItemString + "</div>");
+        // console.log(item.logo_url);
+        // var logo = $(".logo", listItem);
+        // logo.attr("src", item.image);
+        var listItemTitle = $(".slider-title", listItem);
+        listItemTitle.html(item.symbol.toString().toUpperCase() + "/USDT");
+        // console.log(typeof item.current_price);
+        var listItemAmount = $(".slider-price", listItem);
+        listItemAmount.html("$ " + parseFloat(item.current_price).toFixed(2));
+        //var listItemDesc = $(".symbol", listItem);
+        //listItemDesc.html(item.symbol.toString().toUpperCase());
+        var listItemChange = $(".slider-price-change", listItem);
+        listItemChange.html(
+          parseFloat(item.price_change_percentage_24h_in_currency).toFixed(2) + "%"
+        );
+        if (parseFloat(item.price_change_percentage_24h_in_currency).toFixed(2) > 0) {
+          listItemChange.css("color", "green");
+        } else {
+          listItemChange.css("color", "red");
+        }
+        listItem.addClass("slider-item");
+        $(".slider").append(listItem);
+      }
+    });
+
+  </script>
 </body>
 
 </html>';
