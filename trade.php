b9d7dd735dc8ea59da82f8353af27347a61783e1
adityapanchal10 <panchaladitya007@gmail.com>
2022-02-04 00:30:46 +0530

    fixed typo

--- trade.php
+++ trade.php
@@ -532,7 +532,7 @@ if (!isset($_SESSION['email']) || isset($_SESSION['isVerified'])) {
             <!-- Navbar -->
             <nav class="navbar navbar-expand-lg " color-on-scroll="500">
                 <div class="container-fluid">
-                    <a class="navbar-brand" href="#pablo"> Dashboard </a>
+                    <a class="navbar-brand" href="#pablo"> Trade </a>
                     <button href="" class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse"
                         aria-controls="navigation-index" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-bar burger-lines"></span>
