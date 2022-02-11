8f20979c10d84a73f4a14a4f1cf6e6eefaeb2dc2
adityapanchal10 <panchaladitya007@gmail.com>
2022-02-12 01:18:18 +0530

    changed nucleo icons in sidebar

--- dashboard.php
+++ dashboard.php
@@ -252,13 +252,13 @@ if (!isset($_SESSION['email']) || isset($_SESSION['isVerified'])) {
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="trade.php">
-                            <i class="nc-icon nc-circle-09"></i>
+                            <i class="nc-icon nc-money-coins"></i>
                             <p>Trade</p>
                         </a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="holdings.php">
-                            <i class="nc-icon nc-circle-09"></i>
+                            <i class="nc-icon nc-spaceship"></i>
                             <p>Holdings</p>
                         </a>
                     </li>
@@ -272,7 +272,7 @@ if (!isset($_SESSION['email']) || isset($_SESSION['isVerified'])) {
                 <ul class="nav">
                     <li class="nav-item">
                         <a class="nav-link" href="contact.php">
-                            <i class="nc-icon nc-chart-pie-35"></i>
+                            <i class="nc-icon nc-badge"></i>
                             <p>Contact Us</p>
                         </a>
                     </li>
